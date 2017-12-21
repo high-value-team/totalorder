fa238686610e565121ddd806a4c00728f4e8f0dc
Ralf Westphal <info@ralfw.de>
2017-12-21 20:34:51 +0100

    Testprojekt für RESTController aufgesetzt

---
+++ backend/src/to.backend/to.backend.tests/test_RESTController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace to.backend.tests
+{
+    [TestFixture]
+    public class test_RESTController
+    {
+        [Test]
+        public void Version()
+        {
+            
+        }
+    }
+}
