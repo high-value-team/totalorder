d2ddeb2b1fa0d42947fb92ea71a59909b0e2c4e7
Ralf Westphal <info@ralfw.de>
2018-01-24 10:04:36 +0100

    backend verzeichnisse refaktorisiert

--- backend/deploy/deploy-dropstack.sh
+++ backend/deploy/deploy-dropstack.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 source dropstack/environment.sh
 
-rm -fr ../deployed.dropstack
-mkdir ../deployed.dropstack
-cp ../bin/*.* ../deployed.dropstack
-cp dropstack/.dropstack.json ../deployed.dropstack
-cp dropstack/Dockerfile ../deployed.dropstack
+rm -fr tmp.deployed.dropstack
+mkdir tmp.deployed.dropstack
+cp ../bin/*.* tmp.deployed.dropstack
+cp dropstack/.dropstack.json tmp.deployed.dropstack
+cp dropstack/Dockerfile tmp.deployed.dropstack
 
-cd ../deployed.dropstack
+cd tmp.deployed.dropstack
 dropstack deploy --compress --verbose --alias totalorder-backend.cloud.dropstack.run --type mono --stateful --token $DROPSTACK_TOKEN
