0df3bf5633b936604c58c557c453d0b2e7badeca
Florian Boehmak <florian@fnbk.cc>
2018-04-11 12:19:17 +0200

    remove docker shell scripts

--- backend/build/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker run --interactive --env TOTALORDER_BACKEND_DATABASEPATH=/mnt --publish "127.0.0.1:8080:80" --volume ${PWD}/bin:/workdir mono sh << EOF
-mono /workdir/to.backend.exe
-EOF
-
